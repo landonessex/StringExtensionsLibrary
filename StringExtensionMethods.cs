86653f04b0175ffe86b3e12490cbd7745d83eab0
Landon Essex <61153703+landonessex@users.noreply.github.com>
2025-04-02 17:58:13 -0400

    first commit

---
+++ StringExtensionMethods.cs
@@ -0,0 +1,13 @@
+﻿namespace StringExtensions
+{
+    public static class StringExtensionMethods
+    {
+        public static bool StartsWithUpperCase(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                return false;
+
+            return char.IsUpper(str[0]);
+        }
+    }
+}
