ab78a6bc3047e77850a97fa1c183f49d14eaad67
Landon Essex <61153703+landonessex@users.noreply.github.com>
2025-04-02 18:27:15 -0400

    consumer commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using StringExtensions;
+
+string testString1 = "Hello world";
+string testString2 = "hello world";
+
+Console.WriteLine($"'{testString1}' starts with uppercase: {testString1.StartsWithUpperCase()}");
+Console.WriteLine($"'{testString2}' starts with uppercase: {testString2.StartsWithUpperCase()}");
+
+Console.ReadLine();
